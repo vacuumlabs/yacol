bd989293a044a7b57dafcc66370c1ef7531745b1
Tomas Kulich <tomas.kulich@gmail.com>
2016-10-25 10:06:25 +0200

    Fix express-helpers error handling

--- expressHelpers.js
+++ expressHelpers.js
@@ -14,7 +14,6 @@ export function register(app, verb, pattern, reqHandler) {
   })
 }
 
-
 export function* runApp(app) {
   if (!appToChan.has(app)) {
     throw new Error('you should register at least one handler before running')
@@ -27,26 +26,25 @@ export function* runApp(app) {
       onHeaders(res, () => resolve())
     })
 
-    if (!middlewares.has(req)) {
-      middlewares.set(req, [])
+    const myNext = function* (route) {
+      next()
+      let midds = middlewares.get(req)
+      const which = midds.length
+      yield onHeadersPromise
+      if (which >= midds.length) {
+        throw new Error('No non-middleware handler exists for this route')
+      }
+      yield midds[which]
     }
 
-    const myNext = function* (route) {
+    run(function*() {
+      zone.set('request', req)
+      const handle = run(reqHandler, req, res, myNext)
+      if (!middlewares.has(req)) {
+        middlewares.set(req, [])
+      }
       const midds = middlewares.get(req)
       midds.push(handle)
-      next()
-      yield onHeadersPromise
-      // When processing nth middleware, last element of midds is handle of n+1-th middleware.
-      // Last middleware does not need to wait for anyone else, it just estabilishes the above invariant.
-      if (midds[midds.length - 1] !== handle) {
-        yield midds[midds.length - 1]
-        midds.pop()
-      }
-    }
-
-    const handle = run(function*() {
-      zone.set('req', req)
-      run(reqHandler, req, res, myNext)
-    })
+    }).catch((e) => {console.error(e)})
   }
 }
