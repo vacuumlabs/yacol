2ff6fadeba1781439904908197b2d087f321104e
Tomas Kulich <tomas.kulich@gmail.com>
2016-11-19 17:32:16 +0100

    Add merge, mergeNamed, transducer support

--- src/messaging-advanced.js
+++ src/messaging-advanced.js
@@ -1,7 +1,31 @@
 import {runDetached, kill} from './cor'
 import {createChannel} from './messaging'
-import {isChannel, assertChannel} from './utils'
+import {isChannel, assertChannel, isIterable} from './utils'
 import t from 'transducers-js'
+
+function parseChannelAndTransducer(args) {
+  const res = {}
+  for (let arg of args) {
+    if (isChannel(arg)) {
+      res.channel = arg
+    } else if (typeof arg === 'function') {
+      res.transducer = arg
+    } else {
+      console.error('Expected channel or transducer here, got', arg, 'of type', typeof arg)
+      throw new Error('Expected channel or transducer here')
+    }
+  }
+  if (res.channel == null) {
+    res.channel = createChannel()
+  }
+  return res
+}
+
+/* reducer-like putting to channel which can be transduced */
+function putToChannel(channel, msg) {
+  channel.put(msg)
+  return channel
+}
 
 export function mult(source) {
 
@@ -15,29 +39,6 @@ export function mult(source) {
       }
     }
   })
-
-  function parseChannelAndTransducer(args) {
-    const res = {}
-    for (let arg of args) {
-      if (isChannel(arg)) {
-        res.channel = arg
-      } else if (typeof arg === 'function') {
-        res.transducer = arg
-      } else {
-        console.error('Expected channel or transducer here, got', arg, 'of type', typeof arg)
-        throw new Error('Expected channel or transducer here')
-      }
-    }
-    if (res.channel == null) {
-      res.channel = createChannel()
-    }
-    return res
-  }
-
-  function putToChannel(channel, msg) {
-    channel.put(msg)
-    return channel
-  }
 
   function subscribe(...args) {
     const {channel, transducer = null} = parseChannelAndTransducer(args)
@@ -61,22 +62,67 @@ export function mult(source) {
 
 }
 
-export function merge(inputs, output) {
-  if (output == null) {
-    output = createChannel()
+export function merge(inputs, ...args) {
+  const chanToLabel = new Map()
+  if (!isIterable(inputs)) {
+    throw new Error('yacol.merge: first argument must be iterable of channels. Got no iterable')
   }
+  for (let channel of inputs) {
+    if (!isChannel(channel)) {
+      console.error('Non-channel argument: ', channel, '\ntypeof: ', typeof channel)
+      throw new Error('yacol.merge: first argument must be ' +
+        'iterable of channels. Got something no-chanelish here.')
+    }
+    chanToLabel.set(channel, null)
+  }
+  const {channel, transducer = null} = parseChannelAndTransducer(args)
+  const output = channel
   assertChannel(output)
+  return _merge(chanToLabel, false, output, transducer)
+}
+
+export function mergeNamed(inputs, ...args) {
+  const chanToLabel = new Map()
+  if (typeof inputs !== 'object' || inputs == null) {
+    console.error('yacol.mergeNamed: first argument must be no-nullish object. Got: ', inputs,
+      '\ntypeof: ', typeof inputs)
+    throw new Error('yacol.mergeNamed: first argument must be {name: channel} object')
+  }
+  for (let name in inputs) {
+    let channel = inputs[name]
+    if (!isChannel(channel)) {
+      console.error('yacol.mergeNamed: Non-channel argument ', channel, '\ntypeof: ', typeof channel)
+      throw new Error('yacol.mergeNamed: first argument must have channel values')
+    }
+    chanToLabel.set(channel, name)
+  }
+  const {channel, transducer = null} = parseChannelAndTransducer(args)
+  const output = channel
+  assertChannel(output)
+  return _merge(chanToLabel, true, output, transducer)
+}
+
+
+function _merge(inputs, emitLabels, output, transducer) {
+  let putFn = putToChannel
+  if (transducer != null) {
+    putFn = t.toFn(transducer, putFn)
+  }
   for (let key in inputs) {
     assertChannel(inputs[key])
   }
-
-  for (let key in inputs) {
+  for (let [channel, label] of inputs) {
     runDetached(function* () {
-      const msg = yield inputs[key].take()
-      output.put([key, msg])
+      while (true) {
+        const msg = yield channel.take()
+        if (emitLabels) {
+          putFn(output, [label, msg])
+        } else {
+          putFn(output, msg)
+        }
+      }
     })
   }
-
   return output
 }
 
--- tests/messaging-advanced.js
+++ tests/messaging-advanced.js
@@ -1,7 +1,6 @@
 import {run, createChannel,
-  droppingChannel, slidingChannel, mult, kill, merge} from '../dist'
+  droppingChannel, slidingChannel, mult, kill, merge, mergeNamed} from '../dist'
 import {assert} from 'chai'
-//import {resetTimer, timeApprox} from './utils'
 import Promise from 'bluebird'
 import t from 'transducers-js'
 
@@ -11,13 +10,84 @@ describe('merge', () => {
 
     const ch1 = createChannel()
     const ch2 = createChannel()
+    const ch = merge([ch1, ch2])
+
+    run(function*() {
+      ch1.put('a')
+      yield Promise.resolve()
+      ch2.put('b')
+      yield Promise.resolve()
+      ch1.put('c')
+      yield Promise.resolve()
+      ch2.put('d')
+    })
+
+    run(function*() {
+      const msg1 = yield ch.take()
+      const msg2 = yield ch.take()
+      const msg3 = yield ch.take()
+      const msg4 = yield ch.take()
+      assert.deepEqual(msg1, 'a')
+      assert.deepEqual(msg2, 'b')
+      assert.deepEqual(msg3, 'c')
+      assert.deepEqual(msg4, 'd')
+    })
+  }))
+
+  it('accepts transducer', () => run(function*() {
+    const xf = t.partitionBy((x) => {return typeof x === 'string'})
+    const ch1 = createChannel()
+    const ch2 = createChannel()
+    const ch3 = createChannel()
+    const out = createChannel()
+
+
+    const ch = merge([ch1, ch2, ch3], out, xf)
+    assert.equal(ch, out)
+
+    run(function*() {
+      ch1.put(1)
+      yield Promise.resolve()
+      ch2.put(2)
+      yield Promise.resolve()
+      ch3.put('a')
+      yield Promise.resolve()
+      ch1.put(3)
+      yield Promise.resolve()
+      ch2.put(4)
+      yield Promise.resolve()
+      ch3.put('b')
+    })
+
+    run(function*() {
+      const msg1 = yield ch.take()
+      const msg2 = yield ch.take()
+      const msg3 = yield ch.take()
+      assert.deepEqual(msg1, [1, 2])
+      assert.deepEqual(msg2, ['a'])
+      assert.deepEqual(msg3, [3, 4])
+    })
+  }))
+})
+
+describe('mergeNamed', () => {
+
+  it('basics', () => run(function*() {
+
+    const ch1 = createChannel()
+    const ch2 = createChannel()
     const ch3 = createChannel()
 
-    const ch = merge({ch1, ch2, ch3})
+    const ch = mergeNamed({ch1, ch2, ch3})
 
-    ch1.put('a')
-    ch2.put('b')
-    ch3.put('c')
+    run(function*() {
+      ch1.put('a')
+      yield Promise.resolve()
+      ch2.put('b')
+      yield Promise.resolve()
+      ch3.put('c')
+      yield Promise.resolve()
+    })
 
     run(function*() {
       const msg1 = yield ch.take()
